f624618bd1ea0c25b98c9be40d3423b7c7c97972
Michael Pappas <mixalis.pappas23@gmail.com>
2021-05-17 22:48:27 +1000

    start building my seeds.sql

---
+++ seed.sql
@@ -0,0 +1,21 @@
+USE employees_DB;
+
+----- Department Seeds -----
+
+INSERT INTO department (id, name)
+VALUES (1, "Sales");
+
+INSERT INTO department (id, name)
+VALUES (2, "Warehouse");
+
+INSERT INTO department (id, name)
+VALUES (3, "Human Resouces");
+
+INSERT INTO department (id, name)
+VALUES (4, "IT");
+
+INSERT INTO department (id, name)
+VALUES (5, "Accounting");
+
+INSERT INTO department (id, name)
+VALUES (6, "Receptionist");
