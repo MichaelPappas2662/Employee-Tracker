45e9d9f5a4da84ca6ebf4a6f27624d9e32781a6f
Michael Pappas <mixalis.pappas23@gmail.com>
2021-05-17 22:36:27 +1000

    created a Role Table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,13 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+----- Create Role Table -----
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
