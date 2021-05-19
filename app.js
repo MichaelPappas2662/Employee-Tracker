dbfc4df7ef17398bb7b1a792b34d4a45ce95d168
Michael Pappas <mixalis.pappas23@gmail.com>
2021-05-19 15:16:15 +1000

    starting the app code

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const promiseMySql = require("promise-mysql");
+
+// Create the connection to MySQL WorkBench
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_DB'
+});
+
+connection.query = util.promisify(connection.query);
+
+// Begin the application after establishing the connection.
+connection.connect(function (err) {
+    if (err) throw err;
+    initialAction();
+})
