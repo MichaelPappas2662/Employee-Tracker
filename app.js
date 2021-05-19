c11c424120daa0a3b57f172c674fdf1109ae1663
Michael Pappas <mixalis.pappas23@gmail.com>
2021-05-19 15:21:29 +1000

    adding more code

--- app.js
+++ app.js
@@ -17,5 +17,62 @@ connection.query = util.promisify(connection.query);
 // Begin the application after establishing the connection.
 connection.connect(function (err) {
     if (err) throw err;
-    initialAction();
+    startingScreen();
 })
+
+// Ask the user initial action question to figure out what they would like to do.
+const startingScreen = async () => {
+    try {
+        let answer = await inquirer.prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role',
+                'Exit'
+            ]
+        });
+        switch (answer.action) {
+            case 'View Employees':
+                employeeView();
+                break;
+
+            case 'View Departments':
+                departmentView();
+                break;
+
+            case 'View Roles':
+                roleView();
+                break;
+
+            case 'Add Employees':
+                employeeAdd();
+                break
+
+            case 'Add Departments':
+                departmentAdd();
+                break
+
+            case 'Add Roles':
+                roleAdd();
+                break
+
+            case 'Update Employee Role':
+                employeeUpdate();
+                break
+
+            case 'Exit':
+                connection.end();
+                break;
+        };
+    } catch (err) {
+        console.log(err);
+        startingScreen();
+    };
+}
